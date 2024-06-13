6ef2a40a031916a3ff91a57a4180a6c166675225
rexim <reximkut@gmail.com>
2024-06-14 00:17:48 +0700

    Actually working qlock

--- nob.c
+++ nob.c
@@ -1,13 +1,16 @@
 #define NOB_IMPLEMENTATION
 #include "nob.h"
+#define STB_C_LEXER_IMPLEMENTATION
+#include "stb_c_lexer.h"
 
 bool build_exe(Nob_Cmd *cmd, const char *output_path, const char *input_path)
 {
     cmd->count = 0;
     nob_cmd_append(cmd, "cc", "-Wall", "-Wextra");
     nob_cmd_append(cmd, "-o", output_path);
+    nob_cmd_append(cmd, "-include", "time.h");
     nob_cmd_append(cmd, input_path);
-    return  nob_cmd_run_sync(*cmd);
+    return nob_cmd_run_sync(*cmd);
 }
 
 bool compile_quine_blob(const char *output_path, const char *input_path)
@@ -25,7 +28,7 @@ bool compile_quine_blob(const char *output_path, const char *input_path)
         if (sb.items[i] == '?') {
             for (size_t j = 0; j < sb.count; ++j) {
                 switch (sb.items[j]) {
-                    case '\n': fprintf(output, "\\n"); break;
+                    case '\n': fprintf(output, "\\n\"\n\""); break;
                     case '\\': fprintf(output, "\\\\"); break;
                     case '"':  fprintf(output, "\\\""); break;
                     default: fprintf(output, "%c", sb.items[j]);
@@ -43,6 +46,46 @@ bool compile_quine_blob(const char *output_path, const char *input_path)
     return true;
 }
 
+bool is_unconcatable(long token)
+{
+    return (token == CLEX_id || token == CLEX_intlit);
+}
+
+bool format_tokens(const char *output_path, const char *input_path)
+{
+    Nob_String_Builder sb = {0};
+    if (!nob_read_entire_file(input_path, &sb)) return false;
+
+    FILE *output = fopen(output_path, "wb");
+    if (output == NULL) {
+        nob_log(NOB_ERROR, "Could not read file %s: %s", output_path, strerror(errno));
+        return false;
+    }
+
+    stb_lexer l = {0};
+    static char string_store[1024];
+    stb_c_lexer_init(&l, sb.items, sb.items + sb.count, string_store, NOB_ARRAY_LEN(string_store));
+
+    int x = 0;
+    int prev_token = 0;
+    while (stb_c_lexer_get_token(&l)) {
+        int n = l.where_lastchar - l.where_firstchar + 1;
+        if (is_unconcatable(prev_token) && is_unconcatable(l.token)) {
+            fprintf(output, " ");
+        }
+        prev_token = l.token;
+        fprintf(output, "%.*s", n, l.where_firstchar);
+        x += n;
+        if (x >= 80) {
+            fprintf(output, "\n");
+            x = 0;
+        }
+    }
+    fclose(output);
+    nob_log(NOB_INFO, "Generated %s", output_path);
+    return true;
+}
+
 int main(int argc, char **argv)
 {
     NOB_GO_REBUILD_URSELF(argc, argv);
@@ -52,7 +95,8 @@ int main(int argc, char **argv)
     Nob_Cmd cmd = {0};
     if (!build_exe(&cmd, "./build/quine", "quine.c")) return 1;
     if (!build_exe(&cmd, "./build/clock", "clock.c")) return 1;
-    if (!compile_quine_blob("./build/qlock-blob.c", "qlock.c")) return 1;
+    if (!format_tokens("./build/qlock-formatted.c", "qlock.c")) return 1;
+    if (!compile_quine_blob("./build/qlock-blob.c", "./build/qlock-formatted.c")) return 1;
     if (!build_exe(&cmd, "./build/qlock", "./build/qlock-blob.c")) return 1;
 
     return 0;
--- qlock.c
+++ qlock.c
@@ -1,94 +1,59 @@
-#include <stdio.h>
-#include <string.h>
-#include <time.h>
-#include <unistd.h>
-
-#define FONT_COUNT 11
-int font[FONT_COUNT] = {31599,19812,14479,31207,23524,29411,29679,30866,31727,31719,1040};
-#define FONT_ROWS 5
-#define FONT_COLS 3
-#define DIGITS_COUNT 8
-#define DIGITS_PAD 2
-#define DISPLAY_WIDTH (FONT_COLS + DIGITS_PAD)*DIGITS_COUNT
-#define DISPLAY_HEIGHT FONT_ROWS
-
-void pixel(int ch, int x, int y, int digits[DIGITS_COUNT])
+x, y, d[8], i, dx;
+f[] = {31599,19812,14479,31207,23524,29411,29679,30866,31727,31719,1040};
+extern char *s;
+p(int ch)
 {
-    int i = x/(FONT_COLS + DIGITS_PAD);
-    int dx = x%(FONT_COLS + DIGITS_PAD);
-    if (y < FONT_ROWS && dx < FONT_COLS && (font[digits[i]]>>((FONT_ROWS - y - 1)*FONT_COLS + dx))&1) {
-        printf("\033[1;31m%c\033[0m", ch);
-    } else {
+    i = x/(3 + 2);
+    dx = x%(3 + 2);
+    if (i < 8 && y < 5 && dx < 3 && (f[d[i]]>>((5 - y - 1)*3 + dx))&1)
+        printf("\033[1;41;30m%c\033[0m", ch);
+    else
         printf("%c", ch);
-    }
+    if (ch == '\n') {
+        y += 1;
+        x = 0;
+    } else
+        x += 1;
 }
 
 int main()
 {
-    const char *src = "?";
-    size_t src_n = strlen(src);
+    size_t s_n = strlen(s);
     for (;;) {
         time_t t = time(NULL);
         struct tm *tm = localtime(&t);
+        d[0] = tm->tm_hour/10;
+        d[1] = tm->tm_hour%10;
+        d[2] = 10;
+        d[3] = tm->tm_min/10;
+        d[4] = tm->tm_min%10;
+        d[5] = 10;
+        d[6] = tm->tm_sec/10;
+        d[7] = tm->tm_sec%10;
 
-        int digits[DIGITS_COUNT];
-        digits[0] = tm->tm_hour/10;
-        digits[1] = tm->tm_hour%10;
-        digits[2] = 10;
-        digits[3] = tm->tm_min/10;
-        digits[4] = tm->tm_min%10;
-        digits[5] = 10;
-        digits[6] = tm->tm_sec/10;
-        digits[7] = tm->tm_sec%10;
-
-        int x = 0;
-        int y = 0;
-        for (size_t i = 0; i < src_n && y < FONT_ROWS; ++i) {
-            switch (src[i]) {
+        x = 0;
+        y = 0;
+        for (size_t i = 0; i < s_n; ++i) {
+            switch (s[i]) {
                 case 63: {
-                    for (size_t j = 0; j < src_n; ++j) {
-                        switch (src[j]) {
-                            case '\n': {
-                                pixel('\\', x, y, digits);
-                                x += 1;
-                                pixel('n', x, y, digits);
-                                x += 1;
-                            } break;
-                            case '"': {
-                                pixel('\\', x, y, digits);
-                                x += 1;
-                                pixel('\"', x, y, digits);
-                                x += 1;
-                            } break;
-                            case '\\': {
-                                pixel('\\', x, y, digits);
-                                x += 1;
-                                pixel('\\', x, y, digits);
-                                x += 1;
-                            } break;
-                            default: {
-                                pixel(src[j], x, y, digits);
-                                x += 1;
-                            }
+                    for (size_t j = 0; j < s_n; ++j) {
+                        switch (s[j]) {
+                            case '\n': p('\\'); p('n'); p('"'); p('\n'); p('"'); break;
+                            case '"':  p('\\'); p('\"'); break;
+                            case '\\': p('\\'); p('\\'); break;
+                            default: p(s[j]);
                         }
                     }
                 } break;
 
-                case '\n': {
-                    y += 1;
-                    x = 0;
-                    pixel('\n', x, y, digits);
-                } break;
-
-                default: {
-                    pixel(src[i], x, y, digits);
-                    x += 1;
-                }
+                default:
+                    p(s[i]);
             }
         }
 
-        printf("\033[%dA\033[%dD", y, x);
+        printf("\n\033[%dA\033[%dD", y + 1, x);
         sleep(1);
     }
     return 0;
 }
+char *s = "?";
